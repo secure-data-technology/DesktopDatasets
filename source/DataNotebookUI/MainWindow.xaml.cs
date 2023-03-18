1508dbf03faba8b9a30c638b4adae34073e6af72
Jeff Page <infosec@securedatatechnology.com>
2023-03-18 10:41:40 -0400

    Various view changes

--- source/DataNotebookUI/MainWindow.xaml.cs
+++ source/DataNotebookUI/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace DataNotebookUI
         public MainWindow()
         {
             InitializeComponent();
+         //   this.NavigatorView.FilesListBox.ItemsSource =
         }
     }
 }
